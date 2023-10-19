23cfa60b4bb7b6f1883e9306f3e53e4f34615f78
claus <cmrosito@udesa.edu.ar>
2023-10-19 11:10:21 -0300

    Change docker image name.

--- run.bash
+++ run.bash
@@ -1,4 +1,6 @@
 echo roslaunch turtlebot_gazebo turtlebot_world.launch
 echo 
-rocker --x11 --nvidia ros:melodic-turtlebot2 bash
+
+rocker --nvidia --volume $(pwd)/default.rviz:/opt/ros/melodic/share/rviz/default.rviz --x11 ros1-melodic-turtlebot2
+
 
