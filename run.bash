303fc8a3288991191cdbb58510669eec11f623f6
clausqr <cmrosito@udesa.edu.ar>
2024-03-18 15:22:07 -0300

    Update run.bash

--- run.bash
+++ run.bash
@@ -1,6 +1,6 @@
 echo roslaunch turtlebot_gazebo turtlebot_world.launch
 echo 
 
-rocker --nvidia --volume $(pwd)/default.rviz:/opt/ros/melodic/share/rviz/default.rviz --x11 ros1-melodic-turtlebot2
+rocker --volume $(pwd)/default.rviz:/opt/ros/melodic/share/rviz/default.rviz --x11 ros1-melodic-turtlebot2
 
 
